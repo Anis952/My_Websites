615e9d2cd66a868ce95fdb924e7005bf3415a152
Anish Thapa <np03a190146@heraldcollege.edu.np>
2021-09-03 11:27:10 +0545

    changed in urls

--- anish/urls.py
+++ anish/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
 
 urlpatterns = [
-     path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('app1.urls'))
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+# ] urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
